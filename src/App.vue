b83c1f428233f3d5555ef73f77730a5fdbd154e4
khs <khs@123.com>
2024-01-07 19:59:42 +0800

    1.1项目初始化和使用vant完成tabbar布局

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TabsView from './views/tabs/TabsView.vue'
+</script>
 
-<template></template>
+<template>
+  <tabs-view />
+</template>
 
 <style scoped></style>
---
+++ src/views/tabs/TabsView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Tabbar, TabbarItem } from 'vant'
+import { ref } from 'vue'
+//记录当前选择的 tab
+const active = ref('home')
+</script>
+
+<template>
+  <div class="center">{{ active }}</div>
+  <Tabbar v-model="active">
+    <TabbarItem name="home" icon="home-o">首页</TabbarItem>
+    <TabbarItem name="order" icon="bars">订单</TabbarItem>
+    <TabbarItem name="me" icon="contact-o">我的</TabbarItem>
+  </Tabbar>
+</template>
+
+<style>
+.center {
+  text-align: center;
+}
+</style>
