0189c306e8f8b893ab54df35f2d4b34183155311
khs <khs@123.com>
2024-01-09 21:31:59 +0800

    定义hooks-useToggle实现搜索页展示切换

--- src/use/useToggle.ts
+++ src/use/useToggle.ts
@@ -1,6 +1,7 @@
 import { ref } from 'vue'
 import type { Ref } from 'vue'
 
+// 搜索页面切换hook
 export function useToggle(initState: boolean): [Ref<boolean>, () => void] {
   const state = ref(initState)
   const toggle = function () {
