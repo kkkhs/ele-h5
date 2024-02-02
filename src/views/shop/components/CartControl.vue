7dbd5a87f105a7900ff6589088dbd0fa29f9f8a5
khs <khs@123.com>
2024-02-02 22:06:27 +0800

    7.8商品列表和购物车数量组件开发

---
+++ src/views/shop/components/CartControl.vue
@@ -0,0 +1,118 @@
+<script setup lang="ts">
+import type { IGood } from '@/types';
+import { computed } from 'vue'
+import { useCartStore } from '@/stores/cart'
+
+//组件传参
+interface IProps{
+  data: IGood
+}
+const props = defineProps<IProps>()
+
+const store = useCartStore()
+const cartCount = computed(() => store.cartCountById(props.data.id))
+const minus = () => {
+  store.removeProductFromCart(props.data)
+}
+const add = () => {
+  store.pushProductToCart(props.data)
+}
+</script>
+
+<template>
+  <div class="shop-cart-control">
+    <Transition name="move">
+      <div v-if="cartCount" class="minus" @click.stop="minus"></div>
+    </Transition>
+    <Transition name="fade">
+      <div v-if="cartCount" class="count">{{ cartCount  }}</div>
+    </Transition>
+    <div class="add" @click.stop="add"></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.shop-cart-control{
+  display: flex;
+  align-items: center;
+  height: 22px;
+
+  .add {
+    background: var(--op-primary-color);
+    border-radius: 50%;
+    width: 22px;
+    height: 22px;
+    position: relative;
+    &:before{
+      display: block;
+      position: absolute;
+      top:50%;
+      left: 50%;
+      content: '';
+      width: 50%;
+      height: 2px;
+      background: white;
+      border-radius: 2px;
+      transform: translate(-50%, -50%);
+    }
+    &:after{
+      display: block;
+      position: absolute;
+      top:50%;
+      left: 50%;
+      content: '';
+      width: 50%;
+      height: 2px;
+      background: white;
+      border-radius: 2px;
+      transform: translate(-50%, -50%) rotate(90deg);
+    }
+  }
+  .count{
+    width: 30px;
+    text-align: center;
+
+    &.fade-enter-active,
+    &.fade-leave-active{
+      transition: all 0.2s ease-out;
+    }
+    
+    &.fade-enter-from,
+    &.fade-leave-to{
+      opacity: 0;
+    }
+  }
+
+  .minus{
+    background: white;
+    border: 1px solid var(--op-primary-color);
+    border-radius: 50%;
+    width: 22px;
+    height: 22px;
+    box-sizing: border-box;
+    position: relative;
+    &:before{
+      display: block;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      content: '';
+      width: 50%;
+      height: 2px;
+      background: var(--op-primary-color);
+      border-radius: 2px;
+      transform: translate(-50%, -50%);
+    }
+    &.move-enter-active,
+    &.move-leave-active{
+      transition: all 0.3s ease-out;
+    }
+    
+    &.move-enter-from,
+    &.move-leave-to{
+      opacity: 0;
+      transform: translateX(34px) rotate(180deg);
+    }
+  }
+}
+</style>
--- src/views/shop/components/GoodsList.vue
+++ src/views/shop/components/GoodsList.vue
@@ -1,16 +1,504 @@
 <script setup lang="ts">
-import type { IMenu } from '@/types'
-import { ref } from 'vue'
+import type { IGood, IMenu } from '@/types'
+import { ref, watch } from 'vue'
 import OpLoadingView from '@/components/OpLoadingView.vue';
 import { useAsync } from '@/use/useAsync';
+import { useCartStore } from '@/stores/cart'
 import { fetchGoodsListData } from '@/api/goods'
 import { useRoute } from 'vue-router';
+import GoodsItem from './GoodsItem.vue';
 
 const route = useRoute()
 const { id } = route.params
-const { data, pending } = useAsync(
+const { pending } = useAsync(
   () => fetchGoodsListData(id as string).then((v) => v.data), [] as IMenu[])
+const data: IMenu[] = [
+      {
+        value: 1,
+        label: '单品活动',
+        icon: '',
+        goods: [
+          {
+            id: 1,
+            name: '【果切】红柚+芒果 约350g 芒果拼拼',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:1,
+            checked:true
+          },{
+            id: 2,
+            name: '【果切】随机拼 约250g 阳光果切',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 345,
+            rating: 4.2,
+            price:19.9,
+            oldPrice:32.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 3,
+            name: '【果切】',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:15.9,
+            oldPrice:26.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 4,
+            name: '【果切】',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:29.9,
+            oldPrice:38.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          }
+        ]
+      },
+      {
+        value: 2,
+        label: '满减活动',
+        icon: '',
+        goods: [
+          {
+            id: 5,
+            name: '【果切】红柚+芒果 约350g 芒果拼拼',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:1,
+            checked:true
+          },{
+            id: 6,
+            name: '【果切】随机拼 约250g 阳光果切',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 345,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 7,
+            name: '【果切】111',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 8,
+            name: '【果切】222',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          }
+        ]
+      },
+      {
+        value: 3,
+        label: '热销爆款',
+        icon: '',
+        goods: [
+          {
+            id: 9,
+            name: '【果切】红柚+芒果 约350g 芒果拼拼',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:1,
+            checked:true
+          },{
+            id: 10,
+            name: '【果切】随机拼 约250g 阳光果切',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 345,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 11,
+            name: '【果切】111',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 12,
+            name: '【果切】222',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          }
+        ]
+      },
+      {
+        value: 4,
+        label: '其他',
+        icon: '',
+        goods: [
+          {
+            id: 13,
+            name: '【果切】红柚+芒果 约350g 芒果拼拼',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:1,
+            checked:true
+          },{
+            id: 14,
+            name: '【果切】随机拼 约250g 阳光果切',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 345,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 15,
+            name: '【果切】111',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 16,
+            name: '【果切】222',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          }
+        ]
+      }
+      ,
+      {
+        value: 4,
+        label: '其他',
+        icon: '',
+        goods: [
+          {
+            id: 17,
+            name: '【果切】红柚+芒果 约350g 芒果拼拼',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:1,
+            checked:true
+          },{
+            id: 18,
+            name: '【果切】随机拼 约250g 阳光果切',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 345,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 19,
+            name: '【果切】111',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 20,
+            name: '【果切】222',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          }
+        ]
+      }
+      ,
+      {
+        value: 5,
+        label: '热门',
+        icon: '',
+        goods: [
+          {
+            id: 21,
+            name: '【果切】红柚+芒果 约350g 芒果拼拼',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:1,
+            checked:true
+          },{
+            id: 22,
+            name: '【果切】随机拼 约250g 阳光果切',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 345,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 23,
+            name: '【果切】111',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 24,
+            name: '【果切】222',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          }
+        ]
+      }
+      ,
+      {
+        value: 6,
+        label: '热销款',
+        icon: '',
+        goods: [
+          {
+            id: 25,
+            name: '【果切】红柚+芒果 约350g 芒果拼拼',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:1,
+            checked:true
+          },{
+            id: 26,
+            name: '【果切】随机拼 约250g 阳光果切',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 345,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 27,
+            name: '【果切】111',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          },{
+            id: 28,
+            name: '【果切】222',
+            imgUrl: '/imgs/index_page/shop-list/post1.png',
+            sellCount: 587,
+            rating: 4.2,
+            price:9.9,
+            oldPrice:22.9,
+            tips:"",
+            discount:{
+              type:1,
+              limitCount:1,
+            },
+            cartCount:0,
+            checked:false
+          }
+        ]
+      }
+    ]
 
+const { setCartItems } = useCartStore()
+watch(data, (nv) => {
+  const cartGoods = nv.reduce((p: IGood[], v: IMenu) => [...p, ...v.goods], []).filter((v) => v.cartCount)
+  setCartItems(cartGoods)
+})
 const categoryActive = ref(0)
 </script>
 
@@ -23,7 +511,7 @@ const categoryActive = ref(0)
       <div class="list">
         <template v-for="v in data" :key="v.label">
           <div class="category-name">{{ v.label }}</div>
-          <div v-for="cv in v.goods" :key="cv.id">{{ cv.name }}</div>
+          <GoodsItem v-for="cv in v.goods" :key="cv.id" :data="cv"/>
         </template>
       </div>
     </div>
@@ -35,6 +523,7 @@ const categoryActive = ref(0)
   display: flex;
 
   .list{
+    flex: 1;
     margin: 0 10px;
     .category-name {
       font-size: 15px;
